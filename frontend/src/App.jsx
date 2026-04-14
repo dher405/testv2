ab5c827fcad9f3736229668f07a5e7e17a967540
dher405 <david.herron74@gmail.com>
2026-04-13 19:52:07 -0600

    Import and render DogImageRotator in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import Description from './components/Description';
 import ImageGallery from './components/ImageGallery';
+import DogImageRotator from './components/DogImageRotator';
 
 function App() {
   return (
     <div className="bg-slate-900 min-h-screen">
       <Header />
       <Description />
+      <DogImageRotator />
       <ImageGallery />
     </div>
   );
