03bb3e62f8e3c5ca04d89bf63861ae4ea8a2bfbb
dher405 <david.herron74@gmail.com>
2026-04-13 19:48:26 -0600

    Import and render DogImageRotator in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+import DogImageRotator from './components/DogImageRotator';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DogImageRotator />
   </React.StrictMode>
 );
