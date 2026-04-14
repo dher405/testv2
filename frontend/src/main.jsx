02e88fa3e341c62283ae6b4f4f7e7ba84a5af66d
dher405 <david.herron74@gmail.com>
2026-04-13 21:22:00 -0600

    Update main.jsx to render App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import DogImageRotator from './components/DogImageRotator.jsx';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <DogImageRotator />
+    <App />
 );
