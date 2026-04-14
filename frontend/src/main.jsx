219fc5eeb78f0af1c83e81ac18d7c69a1efa1967
dher405 <david.herron74@gmail.com>
2026-04-13 21:22:58 -0600

    Update main.jsx to render DogPictureRotator

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import DogPictureRotator from './components/DogPictureRotator.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
-);
+  React.createElement(React.StrictMode, null,
+    React.createElement(DogPictureRotator))
+)
