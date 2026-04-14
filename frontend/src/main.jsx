d1aa6ee4bdfc68767824293688c4ab05fb6f4b78
dher405 <david.herron74@gmail.com>
2026-04-14 09:04:03 -0600

    Update main.jsx to render App.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Retro80sSynth from './components/Retro80sSynth.jsx'
+import App from './App.jsx'
 ReactDOM.createRoot(document.getElementById('root')).render(
   React.createElement(React.StrictMode, null,
-    React.createElement(Retro80sSynth))
+    React.createElement(App))
 )
