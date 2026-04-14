e1e3eb3e03a21cc2f18f5f3a7e40fe3b44dd2062
dher405 <david.herron74@gmail.com>
2026-04-13 18:06:28 -0600

    Create Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="bg-slate-900 text-slate-200 p-4">
+      <h1 className="text-3xl font-bold">Welcome to Dog Land</h1>
+    </header>
+  );
+}
+
+export default Header;
