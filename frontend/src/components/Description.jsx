b86f2a29d1244beb7162e5220301d25d94f0427d
dher405 <david.herron74@gmail.com>
2026-04-13 18:06:35 -0600

    Create Description component

---
+++ frontend/src/components/Description.jsx
@@ -0,0 +1,12 @@
+function Description() {
+  return (
+    <div className="p-4">
+      <p className="text-slate-200">
+        Dogs are domesticated mammals, not natural wild animals. They are in the same family as wolves.
+        Dogs were the first animals that humans tamed.
+      </p>
+    </div>
+  );
+}
+
+export default Description;
